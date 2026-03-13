d185730f20bd87dc9c362b005c8b1b1dd366562b
Dawid <79448912+kenwaay@users.noreply.github.com>
2026-03-13 20:39:04 +0100

    gitignore and footer text update

--- src/js/components/Footer/Footer.jsx
+++ src/js/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
                 <div className="row">
                     <div className="col-12">
                         <p className="main-footer__text text-center mb-0">
-                            &copy; {new Date().getFullYear()} <a href="https://github.com/gaiusAmogus/webpack_react_gaius_amogus.git">Webpack React Gaius Amogus</a>
+                            &copy; {new Date().getFullYear()} <a href="https://github.com/gaiusAmogus/gaiusAmogus-next.js-wordpress-starter">Gaius Amogus Next.js WordPress Starter</a>
                         </p>
                     </div>
                 </div>
